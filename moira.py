b8c7be612776d839f19e920d66ee9c8a7ea255e7
Brandon Wu <brandonwu@example.com>
2013-03-07 22:37:10 +0000

    Start get portfolio function

--- moira.py
+++ moira.py
@@ -1,8 +1,9 @@
 import requests
 import lxml
 import logging
+from lxml import etree
 
-#Remove the root logger's default output handlers, otherwise we'll see Requests' logs
+#Remove the root logger's default output handler
 root = logging.getLogger()
 if root.handlers:
     for handler in root.handlers:
@@ -19,32 +20,41 @@ formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
-def login(username, password):
-	"""Issues a login request.
+def get_token(username, password):
+	"""Issues a login request, returns auth cookiejar.
 	
 	:param username: the marketwatch.com username (email)
 	:param password: the plaintext marketwatch.com password
 	:returns: Requests cookiejar containing authentication token
 	"""
-	sessionid=requests.get('http://www.marketwatch.com/') #You need to obtain the ASP.NET_SessionId cookie first, which is set by every page except for the login page for some reason.
-	sessionid=sessionid.cookies
-	userdata={"userName":username,"password":password,"remChk":"on","returnUrl":"/user/login/status","persist":"true"}
-	r=requests.post('https://secure.marketwatch.com/user/account/logon', params=userdata, cookies=sessionid) #Pass the SID and user/pass to the account logon endpoint
-	logger.debug(sessionid['.ASPXAUTH'])
-	check=requests.get('http://www.marketwatch.com/user/login/status',cookies=sessionid) #If the login succeeded, it redirects to http://www.marketwatch.com/my
-	if check.url=="http://www.marketwatch.com/my":
-		logger.info("Login succeeded.")
-	else:
-		logger.warn("Authentication failure.")
-	return sessionid
-
-def login2(username, password):
-	s=requests.Session()
+	userdata = {
+		"userName": username,
+		"password": password,
+		"remChk": "on",
+		"returnUrl": "/user/login/status",
+		"persist": "true"
+	}
+	s = requests.Session()
 	s.get('http://www.marketwatch.com/')
-	userdata={"userName":username,"password":password,"remChk":"on","returnUrl":"/user/login/status","persist":"true"}
-	s.post('https://secure.marketwatch.com/user/account/logon', params=userdata)
-	if s.get('http://www.marketwatch.com/user/login/status').url=="http://www.marketwatch.com/my":
+	s.post('https://secure.marketwatch.com/user/account/logon',
+		params=userdata)
+	if s.get('http://www.marketwatch.com/user/login/status').url == \
+		"http://www.marketwatch.com/my":
 		logger.info("Login success.")
 	else:
 		logger.warn("Auth failure.")
 	return s.cookies
+
+def get_portfolio(token, game):
+	"""Downloads and parses current portfolio, returns dict.
+
+	:param token: cookiejar returned by a call to get_token()
+	:param game: the name of the game (marketwatch.com/game/XXXXXXX)
+	:returns: dictionary keyed with stock symbol
+	"""
+	url = "http://www.marketwatch.com/game/" + game + \
+		"/portfolio/holdings?view=list&partial=True"
+	s = requests.Session()
+	response = s.get(url, cookies=token).text
+	tree = etree.fromstring(response)
+	print etree.tostring(tree)
--- readme.md
+++ readme.md
@@ -3,7 +3,7 @@ MOIRA
 
 What is it?
 -----------
-MOIRA, the MOIRA Otto-matic Intelligent Reconniter of Assets is an API for the Marketwatch Virtual Stock Exchange game. It allows the user to buy and sell shares of virtual stock, as well as get the prices of the stock in realtime. This permits one to use quant methods on the Marketwatch VSE - providing a testing ground for algorithms, or merely for the enjoyment of programmatically winning a virtual stock market game.
+*MOIRA*, the <b>M</b>OIRA <b>O</b>tto-matic <b>I</b>ntelligent <b>R</b>econniter of <b>A</b>ssets is an API for the Marketwatch Virtual Stock Exchange game. It allows the user to buy and sell shares of virtual stock, as well as get the prices of the stock in realtime. This permits one to use quant methods on the Marketwatch VSE - providing a testing ground for algorithms, or merely for the enjoyment of programmatically winning a virtual stock market game.
 
 License
 -------
