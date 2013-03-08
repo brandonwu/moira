7e781c34c35eb04511e041a1263bb1fb6fd868c4
Brandon Wu <brandonwu@example.com>
2013-03-08 14:55:48 -0800

    get_stocksdict method complete; current holdings can be obtained

--- moira.py
+++ moira.py
@@ -21,12 +21,10 @@ logger.addHandler(ch)
 
 #Class it up so we'll be able to do portfolio.stock['EXCHANGETRADEDFUND-XASQ-IXJ'].price
 #TODO: implement portfolio class
-#class Portfolio(timeupdated, stocks, cash, leverage, net_worth,
-#		 purchasing_power, starting_cash, return_amt):
-def blah():
+class Portfolio():
 	"""Stores portfolio data.
 
-	:param timeupdated: server time from headers
+	:param time: server time from headers
 	:param stock: dict of stock objects keyed on id
 	:param cash: current cash remaining
 	:param leverage: current amount left under borrowing limit
@@ -35,10 +33,19 @@ def blah():
 	:param starting_cash: cash amount provided at game start
 	:param return_amt: amount of return obtained from trading
 	"""
+	def __init__(self, time, stocks, cash, leverage, net_worth, purchasing_power, 
+		     starting_cash, return_amt):
+		self.time = time
+		self.stocks = stocks
+		self.cash = cash
+		self.leverage = leverage
+		self.net_worth = net_worth
+		self.purchasing_power = purchasing_power
+		self.starting_cash = starting_cash
+		self.return_amt = return_amt
 
-#TODO: implement stock class
-#class Stock(token, id, ticker, security_type, current_price, shares, purchase_type,
-#	     purchase_price, returns):
+#TODO: implement purchase_price handling in Stock class
+class Stock():
 	"""Stores stock data.
 
 	:param token: auth token (used to get purchase price and ROI)
@@ -51,12 +58,16 @@ def blah():
 	:param return: ROI
 	x:param purchase_price: price at time of purchase
 	"""
-
-	def get_purchase_price(token, id):
-		#parse transactions page here for purchase price; alternatively,
-		#calculate it from the provided ROI
-	
-	self.purchase_price = get_purchase_price(id)
+	def __init__(self, id, ticker, security_type, current_price, shares,
+		     purchase_type, returns): #purchase_price
+		self.id = id
+		self.ticker = ticker
+		self.security_type = security_type
+		self.current_price = current_price
+		self.shares = shares
+		self.purchase_type = purchase_type
+#		self.purchase_price = purchase_price
+		self.returns = returns
 
 def get_token(username, password):
 	"""Issues a login request, returns auth cookiejar. Nom nom nom!
@@ -83,8 +94,8 @@ def get_token(username, password):
 		logger.warn("Auth failure.")
 	return s.cookies
 
-def get_portfolio(token, game):
-	"""Downloads and parses current portfolio, returns dict.
+def get_stocksdict(token, game):
+	"""Fetches and parses current holdings, returns dict.
 
 	:param token: cookiejar returned by a call to get_token()
 	:param game: the name of the game (marketwatch.com/game/XXXXXXX)
@@ -97,27 +108,38 @@ def get_portfolio(token, game):
 	time = r.headers['date']
 	response = r.text
 	soup = BeautifulSoup(response)
+	#the trs in portf. page have stock data atributes in the tag
 	trs = soup.find_all('tr')
-	trs.pop(0)
-	security_type = [ x['data-insttype'] for x in trs ]
-	id = [ x['data-symbol'] for x in trs ]
-	ticker = [ x['data-ticker'] for x in trs ]
-	price = [ x['data-price'] for x in trs ]
-	shares = [ x['data-shares'] for x in trs ]
-	purchase_type = [ x['data-type'] for x in trs ]
-	#TODO: get ROI by parsing inner text for TDs...need extra logic
-	#to determine which stock goes with which return data
-	print(time, security_type, id, ticker, price, shares, purchase_type)
+	trs.pop(0) #remove table header row
+	#get tds for current ROI per stock
+	tds = soup.find_all('td', { 'class' : 'marketgain' })
+	#TODO: get purchase_price by parsing orders page
 	#assemble stock objects
 	#stock objects are stored in a dict keyed by the stock id
-	for x in trs:
-		s = Stock()
-		s.id = x['data-symbol']
-		s.ticker = x['data-ticker']
-		s.security_type = x['data-insttype']
-		s.purchase_type = x['data-type']
-		s.shares = x['data-shares']
-		stock_dict[x] = s
-	p = Portfolio()
-	p.time = time
-	p.stocks = stock_dict
+	stock_dict = {}
+	for x,y in zip(trs, tds):
+		stock_dict[x['data-symbol']] = Stock(x['data-symbol'],
+						     x['data-ticker'],
+						     x['data-insttype'],
+						     x['data-price'],
+						     x['data-shares'],
+						     x['data-type'],
+						     y.contents
+						     #TODO: incl purchase price
+						    )
+	#p = Portfolio()
+	#access the data in the stock dict returned like so:
+	#stock_dict['EXCHANGETRADEDFUND-XASQ-IXJ'].current_price
+	return stock_dict
+
+def get_transaction_history(token, game):
+	"""Downloads and parses past transactions, returns Trans object
+
+	:param token: cookiejar returned by get_token
+	:param game: game name, marketwatch.com/game/XXXXXXX
+	"""
+	orders_url = ("http://www.marketwatch.com/game/msj-2013/portfolio/"
+		      "transactionhistory?sort=TransactionDate&descending="
+		      "True&partial=true&index=0")
+	
+def get_portfolio(token, game):
