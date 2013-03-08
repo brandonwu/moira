661aaedd3672224079a78ae8b720967d48f2782e
Brandon Wu <brandonwu@example.com>
2013-03-08 01:35:46 -0800

    Additional prototyping

--- moira.py
+++ moira.py
@@ -1,7 +1,6 @@
 import requests
-import lxml
 import logging
-from lxml import etree
+from bs4 import BeautifulSoup
 
 #Remove the root logger's default output handler
 root = logging.getLogger()
@@ -20,9 +19,48 @@ formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
-def get_token(username, password):
-	"""Issues a login request, returns auth cookiejar.
+#Class it up so we'll be able to do portfolio.stock['EXCHANGETRADEDFUND-XASQ-IXJ'].price
+#TODO: implement portfolio class
+#class Portfolio(timeupdated, stocks, cash, leverage, net_worth,
+#		 purchasing_power, starting_cash, return_amt):
+def blah():
+	"""Stores portfolio data.
+
+	:param timeupdated: server time from headers
+	:param stock: dict of stock objects keyed on id
+	:param cash: current cash remaining
+	:param leverage: current amount left under borrowing limit
+	:param net_worth: sum of assets and liabilities
+	:param purchasing_power: amount (credit + cash) left to purchase
+	:param starting_cash: cash amount provided at game start
+	:param return_amt: amount of return obtained from trading
+	"""
+
+#TODO: implement stock class
+#class Stock(token, id, ticker, security_type, current_price, shares, purchase_type,
+#	     purchase_price, returns):
+	"""Stores stock data.
+
+	:param token: auth token (used to get purchase price and ROI)
+	:param id: unique id assigned by marketwatch to each security
+	:param ticker: stock symbol
+	:param security_type: "ExchangeTradedFund" or "Stock"
+	:param current_price: current price per share
+	:param shares: number of shares held
+	:param purchase_type: "Buy" or "Short"
+	:param return: ROI
+	x:param purchase_price: price at time of purchase
+	"""
+
+	def get_purchase_price(token, id):
+		#parse transactions page here for purchase price; alternatively,
+		#calculate it from the provided ROI
 	
+	self.purchase_price = get_purchase_price(id)
+
+def get_token(username, password):
+	"""Issues a login request, returns auth cookiejar. Nom nom nom!
+
 	:param username: the marketwatch.com username (email)
 	:param password: the plaintext marketwatch.com password
 	:returns: Requests cookiejar containing authentication token
@@ -50,11 +88,36 @@ def get_portfolio(token, game):
 
 	:param token: cookiejar returned by a call to get_token()
 	:param game: the name of the game (marketwatch.com/game/XXXXXXX)
-	:returns: dictionary keyed with stock symbol
+	:returns: portfolio object
 	"""
-	url = "http://www.marketwatch.com/game/" + game + \
-		"/portfolio/holdings?view=list&partial=True"
-	s = requests.Session()
-	response = s.get(url, cookies=token).text
-	tree = etree.fromstring(response)
-	print etree.tostring(tree)
+	url = ("http://www.marketwatch.com/game/%s/portfolio/holdings"
+	"?view=list&partial=True") % game
+	p = requests.Session()
+	r = p.get(url, cookies=token)
+	time = r.headers['date']
+	response = r.text
+	soup = BeautifulSoup(response)
+	trs = soup.find_all('tr')
+	trs.pop(0)
+	security_type = [ x['data-insttype'] for x in trs ]
+	id = [ x['data-symbol'] for x in trs ]
+	ticker = [ x['data-ticker'] for x in trs ]
+	price = [ x['data-price'] for x in trs ]
+	shares = [ x['data-shares'] for x in trs ]
+	purchase_type = [ x['data-type'] for x in trs ]
+	#TODO: get ROI by parsing inner text for TDs...need extra logic
+	#to determine which stock goes with which return data
+	print(time, security_type, id, ticker, price, shares, purchase_type)
+	#assemble stock objects
+	#stock objects are stored in a dict keyed by the stock id
+	for x in trs:
+		s = Stock()
+		s.id = x['data-symbol']
+		s.ticker = x['data-ticker']
+		s.security_type = x['data-insttype']
+		s.purchase_type = x['data-type']
+		s.shares = x['data-shares']
+		stock_dict[x] = s
+	p = Portfolio()
+	p.time = time
+	p.stocks = stock_dict
--- readme.md
+++ readme.md
@@ -3,8 +3,8 @@ MOIRA
 
 What is it?
 -----------
-*MOIRA*, the <b>M</b>OIRA <b>O</b>tto-matic <b>I</b>ntelligent <b>R</b>econniter of <b>A</b>ssets is an API for the Marketwatch Virtual Stock Exchange game. It allows the user to buy and sell shares of virtual stock, as well as get the prices of the stock in realtime. This permits one to use quant methods on the Marketwatch VSE - providing a testing ground for algorithms, or merely for the enjoyment of programmatically winning a virtual stock market game.
+**MOIRA**, the <b>M</b>OIRA <b>O</b>tto-matic <b>I</b>ntelligent <b>R</b>econniter of <b>A</b>ssets, is an API for the Marketwatch Virtual Stock Exchange game. It provides methods to buy and sell stock, as well as get prices of stock in realtime. This permits one to use quant methods on the Marketwatch VSE - providing a testing ground for algorithms, or merely for the enjoyment of programmatically winning a virtual stock market game.
 
 License
 -------
-This program and all accompanying code is licensed by the author under the terms of the [WTFPL](http://www.wtfpl.net/).
+This program (and all accompanying code) is licensed by the author under the terms of the [WTFPL](http://www.wtfpl.net/).
